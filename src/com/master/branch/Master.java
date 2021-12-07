827672757e86b8e7ac39e3f0bcb9783733e6c2d6
Chetan Kavade <95471470+kawadechetan96@users.noreply.github.com>
2021-12-07 18:13:17 +0530

    Update Master.java

--- src/com/master/branch/Master.java
+++ src/com/master/branch/Master.java
@@ -5,6 +5,9 @@ public class Master {
 	public static void main(String[] args) {
 		
 		System.out.println("Master Branch");
+		System.out.println("Java");
+		System.out.println("Selenium");
+		
 	}
 
 }
